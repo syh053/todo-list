e8284fe0ca6906d9499f29d18b23e16a9dfef914
Hua <syh053.gmail.com>
2024-05-01 13:08:42 +0800

    rounter init

--- app.js
+++ app.js
@@ -4,8 +4,48 @@ const port = 3000
 
 
 //setup rounter
-app.get( "/", (req, res) => {
-    res.send("express test.")
+app.get("/", (req, res) => {
+    res.redirect("/todos")    
+})
+
+
+app.get("/todos", (req, res) => {
+    res.send("show all todos")
+})
+
+
+app.get("/todos/:id", (req, res) => {
+    const id = req.params.id
+    res.send(`get todo : ${id}.`)
+})
+
+
+app.get("/todos/new", (req, res) => {
+    const id = req.params.id
+    res.send("create todo")
+})
+
+
+app.post("/todos", (req, res) => {
+    res.send("add todo")
+})
+
+
+app.get("/todos/:id/edit", (req, res) => {
+    const id = req.params.id
+    res.send(`get todo edit : ${id} `)
+})
+
+
+app.put("/todos/:id", (req,res) => {
+    const id = req.params.id
+    res.send(`modify todo : ${id}`)
+})
+
+
+app.delete("/todos/:id", (req, res) => {
+    const id = req.params.id
+    res.send(`delete todo : ${id}`)
 })
 
 
