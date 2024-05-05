0f8910aed3c94e87df440bfd623d1ef460e1e80b
syh053 <syh053@gmail.com>
2024-05-05 09:45:19 +0800

    modify todo migration file

--- migrations/20240504140401-create-todo.js
+++ migrations/20240504140401-create-todo.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Todo123456', {
+    await queryInterface.createTable('Todos', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -10,6 +10,7 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       name: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       createdAt: {
@@ -23,6 +24,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Todo123');
+    await queryInterface.dropTable('Todos');
   }
 };
