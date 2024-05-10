10eb61325623390dc01e858edbea061781519271
syh053 <syh053@gmail.com>
2024-05-11 00:16:21 +0800

    build updating todo function & optimize screen

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
+// 建立 server
 const express = require("express")
 const app = express()
 const port = 3000
 
+//載入套件
 const { engine } = require("express-handlebars")
+const methodOverride = require('method-override')
+
 
 //對所有的 request 進行前置處理
 app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
 
 const db = require("./models")
+const { where } = require("sequelize")
 const Todo = db.Todo
 
 app.engine('.hbs', engine({ extname: '.hbs' }))
@@ -34,9 +41,7 @@ app.get("/todos", (req, res) =>
 )
 
 
-app.get("/todos/new", (req, res) => {
-    res.render("new")
-})
+app.get("/todos/new", (req, res) => res.render("new"))
 
 
 app.get("/todos/:id", (req, res) => {
@@ -63,13 +68,25 @@ app.post("/todos", (req, res) => {
 
 app.get("/todos/:id/edit", (req, res) => {
     const id = req.params.id
-    res.send(`get todo edit : ${id} `)
+    Todo.findOne({
+        where: {  id: id },
+        attributes: ["id","name"],
+        raw: true
+    })
+        .then(todo => res.render("edit", { todo }))
 })
 
 
 app.put("/todos/:id", (req,res) => {
     const id = req.params.id
-    res.send(`modify todo : ${id}`)
+    const name = req.body.name
+    console.log(id)
+    console.log(name)
+    Todo.update( 
+        { name: name },
+        { where: { id: id } }
+    )
+        .then( () => res.redirect(`/todos/${id}`) )
 })
 
 
