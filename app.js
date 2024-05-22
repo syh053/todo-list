48564b770bc81b742d963eb9f3100162bf5ef01c
syh053 <syh053@gmail.com>
2024-05-22 23:52:09 +0800

    use dotenv

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ const message = require("./middlewares/message-handler")
 const  error  = require("./middlewares/error_handler")
 
 
+if ( process.env.NODE_ENV === "development" ){
+    require("dotenv").config()
+}
+
+console.log("SESSION_SECRET :", process.env.SESSION_SECRET)
+
+
 app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
@@ -26,7 +33,7 @@ app.use(methodOverride('_method'))
 
 
 app.use(session({
-    secret: "This is Secret",
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false
 }))
