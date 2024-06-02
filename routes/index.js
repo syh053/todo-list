63aaa723a9e3b8d75f1818e4e6ba91e8dce8e7d0
syh053 <syh053@gmail.com>
2024-06-02 18:12:11 +0800

    add register/login/logout router

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ const router = express.Router()
 
 const todos = require('./todo')
 
+const login = require('./login')
+
 router.use('/todos', todos)
+
+router.use(login)
 
 // setup rounter
 router.get('/', (req, res) => {
--- routes/login.js
+++ routes/login.js
@@ -1,3 +1,24 @@
 const express = require('express')
-
 const router = express.Router()
+
+router.get('/register', (req, res, next) => {
+  res.send('This is Register Page')
+})
+
+router.get('/login', (req, res, next) => {
+  res.send('This is Login Page')
+})
+
+router.post('/users', (req, res, next) => {
+  res.send('This is Users Page')
+})
+
+router.post('/login', (req, res, next) => {
+  res.send('This is Login Page')
+})
+
+router.post('/logout', (req, res, next) => {
+  res.send('This is Logout Page')
+})
+
+module.exports = router
