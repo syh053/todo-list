8916cc98f1ca882129b5d9e298802e77488a24e7
syh053 <syh053@gmail.com>
2024-06-02 21:36:48 +0800

    add register/login/logout handlebars

--- routes/login.js
+++ routes/login.js
@@ -2,15 +2,16 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/register', (req, res, next) => {
-  res.send('This is Register Page')
+  res.render("register")
 })
 
 router.get('/login', (req, res, next) => {
-  res.send('This is Login Page')
+  res.render("login")
 })
 
 router.post('/users', (req, res, next) => {
-  res.send('This is Users Page')
+  const body = req.body
+  res.send(`name : ${body.name} <br> Email : ${body.email} <br> password : ${body.password} <br> confirmPassword : ${body.confirmPassword}`)
 })
 
 router.post('/login', (req, res, next) => {
